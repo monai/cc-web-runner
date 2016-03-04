84e450536422c17cd81c9cc55a2412d92f765aa1
monai <monai@cure.lt>
2016-03-05 00:07:22 +0200

    Exception manager doen't need to be annotated

--- src/main/java/com/github/monai/GenericExceptionMapper.java
+++ src/main/java/com/github/monai/GenericExceptionMapper.java
@@ -3,14 +3,12 @@ package com.github.monai;
 import com.github.monai.entity.ErrorResponse;
 
 import javax.ws.rs.ClientErrorException;
-import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.ExceptionMapper;
 
-@Produces(MediaType.APPLICATION_JSON)
 public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
   @Context
   UriInfo ui;
