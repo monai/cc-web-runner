090c27c0218123e3a6bb7b1cd549ee4970315c80
monai <monai@cure.lt>
2016-03-04 20:03:52 +0200

    Improve error reporting

--- src/main/java/com/github/monai/GenericExceptionMapper.java
+++ src/main/java/com/github/monai/GenericExceptionMapper.java
@@ -2,18 +2,32 @@ package com.github.monai;
 
 import com.github.monai.entity.ErrorResponse;
 
+import javax.ws.rs.ClientErrorException;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.ExceptionMapper;
 
 @Produces(MediaType.APPLICATION_JSON)
 public class GenericExceptionMapper implements ExceptionMapper<Throwable> {
+  @Context
+  UriInfo ui;
 
   @Override
   public Response toResponse(Throwable ex) {
-    return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-            .entity(new ErrorResponse(ex))
+    return Response.status(getStatus(ex))
+            .entity(new ErrorResponse(ex, ui.getQueryParameters().containsKey("debug")))
+            .type(MediaType.APPLICATION_JSON)
             .build();
+  }
+
+  private int getStatus(Throwable ex) {
+    if (ex instanceof ClientErrorException) {
+      return ((ClientErrorException)ex).getResponse().getStatus();
+    } else {
+      return 500;
+    }
   }
 }
--- src/main/java/com/github/monai/entity/ErrorResponse.java
+++ src/main/java/com/github/monai/entity/ErrorResponse.java
@@ -1,12 +1,21 @@
 package com.github.monai.entity;
 
+import com.owlike.genson.annotation.JsonProperty;
+
+import javax.ws.rs.core.Response;
+
 public class ErrorResponse extends AbstractResponse {
   public final String message;
   public final Throwable exception;
 
-  public ErrorResponse(Throwable exception) {
+  public ErrorResponse(@JsonProperty("exception") Throwable ex,
+                       boolean debug) {
     super(StatusCode.ERROR);
-    this.message = exception.getMessage();
-    this.exception = exception;
+
+    String message = ex.getMessage();
+    this.message = (null != message)
+            ? message
+            : Response.Status.INTERNAL_SERVER_ERROR.toString();
+    this.exception = debug ? ex : null;
   }
 }
