435d96cd2d8938bef789b00d6be88bdb02054ba9
monai <monai@cure.lt>
2016-03-08 10:56:48 +0200

    Run as standalone app

--- src/main/java/com/github/monai/Application.java
+++ src/main/java/com/github/monai/Application.java
@@ -6,7 +6,11 @@ import com.github.monai.converter.SourceFileConverter;
 import com.owlike.genson.Genson;
 import com.owlike.genson.GensonBuilder;
 import com.owlike.genson.ext.jaxrs.GensonJaxRSFeature;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.servlet.ServletContainer;
 
 import javax.ws.rs.ApplicationPath;
 
@@ -25,5 +29,29 @@ public class Application extends ResourceConfig {
     packages("com.github.monai.resource");
     register(new GensonJaxRSFeature().use(genson));
     register(GenericExceptionMapper.class);
+  }
+
+  public static void main(String[] args) throws Exception {
+    Server server = new Server(8080);
+    Application app = new Application();
+
+    ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+    context.setContextPath("/");
+    server.setHandler(context);
+
+    ServletContainer servletContainer = new ServletContainer(app);
+    ServletHolder servlet = new ServletHolder(servletContainer);
+    context.addServlet(servlet, "/*");
+
+    servlet.setInitParameter(
+            "jersey.config.server.provider.classnames",
+            Application.class.getCanonicalName());
+
+    try {
+      server.start();
+      server.join();
+    } finally {
+      server.destroy();
+    }
   }
 }
