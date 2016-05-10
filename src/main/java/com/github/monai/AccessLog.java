173e80931f6c05d1d44610a9176a3507d0ac4072
monai <monai@cure.lt>
2016-05-10 14:30:11 +0300

    Pretty format Response object on info level

--- src/main/java/com/github/monai/AccessLog.java
+++ src/main/java/com/github/monai/AccessLog.java
@@ -3,11 +3,27 @@ package com.github.monai;
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.RequestLog;
 import org.eclipse.jetty.server.Response;
+import org.pmw.tinylog.Level;
 import org.pmw.tinylog.Logger;
 
 public class AccessLog implements RequestLog {
   @Override
   public void log(Request request, Response response) {
     Logger.info(request);
+
+    if (Logger.getLevel().compareTo(Level.DEBUG) == 1) {
+      Logger.info(formatResponse(response));
+    } else {
+      Logger.debug(response.toString());
+    }
+  }
+
+  private String formatResponse(Response response) {
+    return String.format("%s[%s %d %s]@%x",
+            response.getClass().getSimpleName(),
+            response.getHttpChannel().getRequest().getHttpVersion(),
+            response.getStatus(),
+            response.getReason(),
+            response.hashCode());
   }
 }
